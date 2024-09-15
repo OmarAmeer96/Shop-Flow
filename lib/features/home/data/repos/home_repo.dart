c7de91ac3a451f65c4d30e75d6dd14806c907913
OmarAmeer96 <omar.ameer244@gmail.com>
2024-09-15 16:48:55 +0300

    Implemented the HomeRepo.

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,19 @@
+import 'package:shop_flow/core/networking/api_error_handler.dart';
+import 'package:shop_flow/core/networking/api_result.dart';
+import 'package:shop_flow/core/networking/api_service.dart';
+import 'package:shop_flow/features/home/data/models/product.dart';
+
+class HomeRepo {
+  final ApiService _apiService;
+
+  HomeRepo(this._apiService);
+
+  Future<ApiResult<Product>> getAllProducts() async {
+    try {
+      final response = await _apiService.getAllProducts();
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+}
